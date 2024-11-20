0bdc4363cef970c78dd4c24d0e7b413000dc4f2c
merten5 <b3njam1nm3rt3n5@gmail.com>
2024-11-20 15:36:28 -0600

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #Need to change this to our actual db
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+
+def __repr__(self):
+  return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
