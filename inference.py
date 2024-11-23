f88748dd26e94996ea2314c8bea6856e6a5bed63
Katerina Schulz <katerina@fermi.lan>
2024-11-23 14:52:16 -0500

    updates to inference pipeline

--- inference.py
+++ inference.py
@@ -1,5 +1,4 @@
 import torch
-from torch import nn
 import pandas as pd
 import pickle 
 import argparse
@@ -9,13 +8,14 @@ def load_model(model_path):
     if model_path.endswith(".pth"):
         model = torch.load(model_path)
     elif model_path.endswith(".pkl"):
-        model = pickle.load(model_path)
+        with open(model_path , 'rb') as f:
+            model = pickle.load(f)
     else:
         raise TypeError(f"File type {model_path.split('.')[-1]} cannot be loaded.")
     return model
 
 def inference(model, data):
-    if isinstance(model, nn.Module):
+    if isinstance(model, torch.nn.Module):
         logits = model(data)
         _, pred = torch.max(logits, 1)
         return pred
@@ -41,7 +41,7 @@ def parse_data(data):
         'Day_of_Week': 'DayOfWeek',
     })
 
-    df[['Reporting_Airline', 'Flight_Number_Reporting_Airline']] = df['Flight_Info'].str.split(' ', expand=True)
+    df[['Reporting_Airline', 'Flight_Number_Reporting_Airline']] = df['Flight Number'].str.split(' ', expand=True)
     df['Flight_Number_Reporting_Airline'] = df['Flight_Number_Reporting_Airline'].astype(float)
 
     df['Departure Date'] = pd.to_datetime(df['Departure Date'])
@@ -54,7 +54,7 @@ def parse_data(data):
     df['ArrTime'] = df['ArrTime'].dt.hour * 100 + df['ArrTime'].dt.minute + 0.0
 
     df = df.drop(columns=[
-        "Flight_Info",
+        "Flight Number",
         "Departure Date",
         "Departure Time",
         "Airplane",
@@ -65,8 +65,9 @@ def parse_data(data):
         "Arrival Longitude"
     ])
 
-    with open('all_mappings.json', 'r') as f:
-        mapping = json.load('mapping.json') # make sure this file exists somewhere accessible. May need to change path
+    with open('mapping.json', 'r') as f:
+        mapping = json.load(f) # make sure this file exists somewhere accessible. May need to change path
+
 
     df['Origin'] = df['Origin'].map(mapping['Origin']).fillna(-1).astype(int) 
     df['Dest'] = df['Dest'].map(mapping['Dest']).fillna(-1).astype(int) 
@@ -75,18 +76,25 @@ def parse_data(data):
     df['OriginStateName'] = df['OriginStateName'].map(mapping['OriginStateName']).fillna(-1).astype(int) 
     df['DestStateName'] = df['DestStateName'].map(mapping['DestStateName']).fillna(-1).astype(int) 
 
+    desired_order = [
+    'Origin', 'Dest', 'Month', 'AirTime', 'Reporting_Airline', 
+    'Flight_Number_Reporting_Airline', 'DepTimeBlk', 'ArrTime', 
+    'OriginStateName', 'DestStateName', 'DayofMonth', 'DayOfWeek'
+    ]
+    df = df[desired_order]
+
     return df
 
-def main(model_path, data):
-    data = parse_data(data)
-    model = load_model(model_path)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_path", type=str, required=True,)
+    parser.add_argument("-d", "--data", type=str, required=True,)
+    args = parser.parse_args()
+
+    data = parse_data(args.data)
+    model = load_model(args.model_path)
     output = inference(model, data)
     return format(output)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("model_path")
-    parser.add_argument("data")
-    args = parser.parse_args()
-    
-    main(args)
+    main()
